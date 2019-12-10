939de7798484ccfcc3bf3b863ad0c741975ae75f
Derek Jass <jazzer36@gmail.com>
2019-12-10 14:45:23 -0600

    More building blocks.

--- build.py
+++ build.py
@@ -1,6 +1,24 @@
 from jinja2 import Environment, FileSystemLoader
+from yaml import load
+import sass
 
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(trim_blocks=True,
+                  lstrip_blocks=True,
+                  loader=FileSystemLoader('templates'))
+
+with open('cards.yaml') as f:
+    cards = load(f)
+
+with open('relics.yaml') as f:
+    relics = load(f)
+
+with open('potions.yaml') as f:
+    potions = load(f)
+
+with open('enemies.yaml') as f:
+    enemies = load(f)
 
 env.get_template('index.html').stream().dump('docs/index.html')
-env.get_template('cards.html').stream().dump('docs/cards.html')
+env.get_template('cards.html').stream(cards=cards).dump('docs/cards.html')
+
+sass.compile(dirname=('.', 'docs/css'))
